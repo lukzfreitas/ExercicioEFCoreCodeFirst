d8699e9924c68eeaca65ac6a4b0783d40ed4a653
lukzfreitas <lucasrgs17@gmail.com>
2018-08-29 01:20:39 -0300

    primeiro commit

---
+++ PL/MovieContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using T1.ExercicioEFCoreCodeFirst.PL;
+
+namespace ExercicioEFCoreCodeFirst
+{
+    public class MovieContext : DbContext
+    {
+        public static readonly LoggerFactory FabricaLogger = new LoggerFactory(new[] { new
+ConsoleLoggerProvider((_, __) => true, true) });
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+             //.UseLoggerFactory(FabricaLogger)
+            .UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=DBMovieCF;Trusted_Connection=True;");
+        }
+    }
+}
